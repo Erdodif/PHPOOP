19fbaabacec4bf1416c189201907de375ae8e71e
Your Name <you@example.com>
2021-10-08 13:56:41 +0200

    debug

--- index.php
+++ index.php
@@ -171,27 +171,27 @@ class Autok{
     <form method="POST">
         <div>
             <label for="auto_nev">Autó neve: </label>
-            <input type="text" id="auto_nev" name="auto_nev" value="<?php echo $nev;?>">
+            <input type="text" id="auto_nev" name="auto_nev" value="<?php echo $ujnev;?>">
         </div>
         <div>
             <label for="auto_hengerSzam">Hengerek Száma:</label>
-            <input type="text" id="auto_hengerSzam" name="auto_hengerSzam" value="<?php echo $hengerSzam;?>">
+            <input type="text" id="auto_hengerSzam" name="auto_hengerSzam" value="<?php echo $ujhengerSzam;?>">
         </div>
         <div>
             <label for="auto_hengerTerfogat">Hegerek Térfogata:</label>
-            <input type="text" id="auto_hengerTerfogat" name="auto_hengerTerfogat" value="<?php echo $hengerTerfogat;?>">
+            <input type="text" id="auto_hengerTerfogat" name="auto_hengerTerfogat" value="<?php echo $ujhengerTerfogat;?>">
         </div>
         <div>
             <label for="auto_teljesitmeny">Teljesítmény:</label>
-            <input type="text" id="auto_teljesitmeny" name="auto_teljesitmeny" value="<?php echo $teljesitmeny;?>">
+            <input type="text" id="auto_teljesitmeny" name="auto_teljesitmeny" value="<?php echo $ujteljesitmeny;?>">
         </div>
         <div>
             <label for="auto_maxSebesseg">Maximális Sebesség:</label>
-            <input type="text" id="auto_maxSebesseg" name="auto_maxSebesseg" value="<?php echo $maxSebesseg;?>">
+            <input type="text" id="auto_maxSebesseg" name="auto_maxSebesseg" value="<?php echo $ujmaxSebesseg;?>">
         </div>
         <div>
             <label for="auto_fogyasztas">Átlag fogyasztás:</label>
-            <input type="text" id="auto_fogyasztas" name="auto_fogyasztas" value="<?php echo $fogyasztas;?>">
+            <input type="text" id="auto_fogyasztas" name="auto_fogyasztas" value="<?php echo $ujfogyasztas;?>">
         </div>
         <div>
             <label for="auto_kep">Kép:</label>
